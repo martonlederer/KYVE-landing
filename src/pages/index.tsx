5dea657d6145901da2617e364401c5f6f0a67fad
Marton Lederer <marton@lederer.hu>
2021-04-23 00:53:04 +0200

    feat: partners wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -174,62 +174,20 @@ const Home = () => {
           }}
         />
       </div>
-      <Page>
-        <div
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translateX(-50%) translateY(-50%)",
-          }}
-        >
-          <Link
-            href={`${router.asPath}about`}
-            style={{ margin: 0 }}
-            onMouseEnter={() => setLogoHovered(true)}
-            onMouseLeave={() => setLogoHovered(false)}
-          >
-            <svg
-              height="45"
-              viewBox="0 0 1510 350"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M66 350H-9.49949e-08L74.5 -2.38419e-05H140.5L110 143.5H122L235 -2.38419e-05H312V5.99999L179 172V181L263 347V350H190L117 206.5H96L66 350ZM740.152 5.99999L595.152 227H580.152L554.152 350H488.152L514.152 227H499.152L444.152 -2.38419e-05H514.152L552.152 167H561.152L668.152 -2.38419e-05H740.152V5.99999ZM844.305 -2.38419e-05H911.305L922.805 317H931.805L1066.3 -2.38419e-05H1135.3V5.99999L984.305 350H861.305L844.305 -2.38419e-05ZM1229.46 344L1302.46 -2.38419e-05H1509.96V63H1354.96L1337.96 143H1473.46V206H1324.46L1307.46 287H1454.96V350H1229.46V344Z"
-                fill="#F5F5F5"
-              />
-            </svg>
-          </Link>
-          <Spacer y={0.5} />
-          <Text h3 type="secondary" style={{ textTransform: "uppercase" }}>
-            the unified archive for blockchains.
-          </Text>
-          <Spacer y={1} />
-          <Divider>Partners</Divider>
-          <Grid.Container gap={3} justify="center">
+      <div className={styles.Section + " " + styles.SmallSection + " " + styles.PartnerSection}>
+        <div className={styles.SectionContent}>
+          <h2>Partners</h2>
+          <h1>Our users are our<br />backers.</h1>
+          <Spacer y={2} />
+          <div className={styles.Partners}>
             {Partners.map((partner) => (
-              <Grid key={partner.name} style={{ paddingBottom: 0 }}>
-                <Link target="_blank" href={partner.link}>
-                  <Logo name={partner.name} />
-                </Link>
-              </Grid>
+              <Link target="_blank" href={partner.link} key={partner.name}>
+                <Logo name={partner.name} />
+              </Link>
             ))}
-          </Grid.Container>
+          </div>
         </div>
-        <div
-          style={{
-            position: "absolute",
-            top: "95%",
-            left: "50%",
-            transform: "translateX(-50%) translateY(-95%)",
-          }}
-        >
-          <Link target="_blank" href="mailto:team@kyve.network" underline>
-            <Text type="secondary">team@kyve.network</Text>
-          </Link>
-        </div>
-      </Page>
+      </div>
     </>
   );
 };
