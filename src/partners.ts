66317e2788bceca28b8f4333dec198ec31361e60
John Letey <johnletey@gmail.com>
2021-04-30 16:59:55 +0100

    chore: update

--- src/partners.ts
+++ src/partners.ts
@@ -1,7 +1,7 @@
 const Partners = [
   { name: "Arweave", link: "https://www.arweave.org" },
   { name: "Polkadot", link: "https://polkadot.network" },
-  { name: "TheGraph", link: "https://thegraph.com" },
+  // { name: "TheGraph", link: "https://thegraph.com" },
   { name: "Solana", link: "https://solana.com" },
   { name: "Cosmos", link: "https://cosmos.network" },
   { name: "Avalanche", link: "https://www.avalabs.org" },
